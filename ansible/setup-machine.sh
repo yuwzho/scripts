06448ba690bc147f78ce0d53d6d633af83548547
Yuwei Zhou <yuwzho@microsoft.com>
2018-05-15 13:24:31 +0800

    Update setup-machine.sh

--- ansible/setup-machine.sh
+++ ansible/setup-machine.sh
@@ -10,8 +10,7 @@ resourceGroup=$5
 # check whether in root
 
 # install user env
-apt-get intall python-pip=9.0.1
-apt-get update && apt-get install -y libssl-dev libffi-dev python-dev docker.io
+apt-get update && apt-get install -y libssl-dev libffi-dev python-dev python-pip docker.io
 pip install pip --upgrade
 pip install ansible[azure]
 
